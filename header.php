b2739d38543447527d7efe1a5e981141325a8ddf
Connor Meehan <cmeehan@elon.edu>
2019-06-18 15:42:20 -0400

    removing user options from header

--- header.php
+++ header.php
@@ -2,29 +2,29 @@
 <html>
 	<head>
 		<?php 
+		include('partials/session.php');
+		include('partials/definitions.php');
 		include('partials/meta.php');
-		include('partials/tags.php');
+		//include('partials/tags.php');
 		include('partials/scripts.php'); 
 		include('partials/styles.php');
 		?>        
-		
 		<title>Utterfare</title>
 
 	</head>
-	<body>
+	<body ng-app="utterfare">
 		<nav class="navbar navbar-expand-md navbar-light bg-light">
+			<a class="navbar-brand" href="http://localhost/utterfare">
+				<img  class="profile-image__small" src="<?php echo BASE_URL; ?>assets/img/favicon.ico"/>
+			</a>
 		    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		    	<img src="assets/img/fast-food-99180_640.png" alt="" width="25px" height="25px"/>
+		    	      <span class="navbar-toggler-icon"></span>
+
 		    </button>
-			<form class="form-inline my-2 my-lg-0 search-form">
-				<input class="form-control mr-sm-2 search-form__input" type="search" placeholder="Search" aria-label="Search">
+			<form novalidate class="form-inline my-2 my-lg-0 search-form" ng-submit="search(search)" ng-controller="SearchController">
+				<input class="form-control mr-sm-2 search-form__input" type="search" placeholder="Search" aria-label="Search" ng-model="search.terms">
 			</form>
-		    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-		        <ul class="navbar-nav ml-auto">
-	                <li class="nav-item"><a class="nav-link" href="login">Saved</a></li>
-	                <li class="nav-item"><a class="nav-link" href="#"><img class="profile-image__small" src="assets/img/favicon.ico" alt="User profile picture"></a></li>
-	            </ul>
-		    </div>
+
 		</nav>
 
 
@@ -50,3 +50,4 @@ function userHeader($title) {
 		</nav>
     <?php
 }
+include('partials/loading.php');
