7d124b30bed35dfe816b9b30dc35bc21a19add87
Connor Meehan <cmeehan@elon.edu>
2019-06-18 15:43:00 -0400

    Adding user options - initial commit on this branch

--- header.php
+++ header.php
@@ -24,7 +24,27 @@
 			<form novalidate class="form-inline my-2 my-lg-0 search-form" ng-submit="search(search)" ng-controller="SearchController">
 				<input class="form-control mr-sm-2 search-form__input" type="search" placeholder="Search" aria-label="Search" ng-model="search.terms">
 			</form>
-
+			
+		    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		        <ul class="navbar-nav ml-auto">
+	                <li class="nav-item"><a class="nav-link" href="login">Saved</a></li>
+	                <li class="nav-item dropdown">
+	                	<a class="nav-link dropdown-toggle" id="navbarUserDropdownMenuLink" role="button" data-toggle="dropdown"  aria-haspopup="true" aria-expanded="false" href="#"><img class="profile-image__small" src="assets/img/favicon.ico" alt="User profile picture"></a>
+		                <?php if($is_signed_in): ?>
+			                <div class="dropdown-menu dropdown-menu-right user-signed-in" aria-labelledby="navbarUserDropdownMenuLink">
+				                <a class="dropdown-item" href="#">Account</a>
+				                <a class="dropdown-item" href="" onClick="return userSignOut()">Sign Out</a>
+			                </div>
+		                <?php else: ?>
+			                <div class="dropdown-menu dropdown-menu-right user-not-signed-in" aria-labelledby="navbarUserDropdownMenuLink">
+				                <a class="dropdown-item" href="#signInModal" data-toggle="modal">Sign In</a>
+				                <a class="dropdown-item" href="#!/sign-up">Sign Up</a>
+			                </div>
+		                <?php endif; ?>
+	                </li>
+	            </ul>
+		    </div>
+		    
 		</nav>
 
 
