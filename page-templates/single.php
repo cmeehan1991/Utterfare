771a861affc4115b8f3c1c4d3795ab70a5c5fe9a
Connor Meehan <cmeehan@elon.edu>
2019-06-19 16:40:23 -0400

    Remove user reviews section

--- page-templates/single.php
+++ page-templates/single.php
@@ -34,48 +34,4 @@
 		</div>
 	</div>
 
-	<!-- Rating and review section --> 
-	<div class="row">
-		<div class="col-md-12 mx-auto">
-			<h3>Rating</h3>
-			<ul class="item-rating">
-				<li class="item-rating--star" data-rating="1"><i class="far fa-star"></i></li>
-				<li class="item-rating--star" data-rating="2"><i class="far fa-star"></i></li>
-				<li class="item-rating--star" data-rating="3"><i class="far fa-star"></i></li>
-				<li class="item-rating--star" data-rating="4"><i class="far fa-star"></i></li>
-				<li class="item-rating--star" data-rating="5"><i class="far fa-star"></i></li>
-				<li class="item-rating--number"></li>
-			</ul>
-			<h3>Reviews</h3>
-			<ul class="item-reviews"></ul>
-			
-			<?php
-			if(SIGNED_IN): ?>
-			<div class="item-review">
-				<form name="item-review-form" ng-submit="submitItemReview">
-					<div class="form-group">
-						<label for="item-rating">Overall Rating</label>
-						<ul class="item-rating">
-							<li class="item-rating--star" data-rating="1"><i class="far fa-star"></i></li>
-							<li class="item-rating--star" data-rating="2"><i class="far fa-star"></i></li>
-							<li class="item-rating--star" data-rating="3"><i class="far fa-star"></i></li>
-							<li class="item-rating--star" data-rating="4"><i class="far fa-star"></i></li>
-							<li class="item-rating--star" data-rating="5"><i class="far fa-star"></i></li>
-							<li class="item-rating--number"></li>
-						</ul>
-					</div>
-					<div class="form-group">
-						<label for="form-title">Add a headline</label>
-						<input type="text" name="review-title" class="form-control" placholder="Enter a title for your review">
-					</div>
-					<div class="form-group">
-						<label for="form-text">Write your review</label>
-						<textarea name="review-text" class="form-control" rows="3"></textarea>
-					</div>
-					<button type="submit" class="btn btn-secondary">Submit</button>
-				</form> 
-			</div>
-			<?php endif; ?>
-		</div>
-	</div>	
 </div>
