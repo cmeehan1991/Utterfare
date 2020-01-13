8077dbe2367a37f3d4ffde8b5978e501a645038e
Connor Meehan <cmeehan@elon.edu>
2020-01-13 11:36:06 -0500

    Removing Vendor directory

--- vendor/partials/assets.php
+++
@@ -1,2 +0,0 @@
-<link href="assets/styles/css/dist/app.css" rel="stylesheet">
-<script src="assets/js/global/dist/app.js" type="text/javascript"></script>	
--- vendor/vendor.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-include 'header.php';
-
-?>
-<div class="content" ng-view></div>
-<?php include_once('footer.php'); ?>
