5dcc4dbebb728fa2887fee2d9e7be8831298dc46
Connor Meehan <cmeehan@elon.edu>
2020-01-14 09:51:34 -0500

    Updating assets to use minified scripts and styles on production

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const rename = require('gulp-rename');
+const ngAnnotate = require('gulp-ng-annotate');
+const cleanCSS = require('gulp-clean-css');
 
 function clean(cb){
 	cb();
@@ -14,8 +16,11 @@ function build(cb){
 
 function minify_js(cb){
 	
-	gulp.src('assets/js/global/src/*.js')
+	gulp.src('assets/js/global/dist/app.js')
 	.pipe(concat('allscripts.js'))
+	.pipe(ngAnnotate({
+		add:true
+	}))
 	.pipe(gulp.dest('assets/js/global/dist'))
 	.pipe(rename('allscripts.min.js'))
 	.pipe(uglify())
@@ -25,10 +30,16 @@ function minify_js(cb){
 }
 
 function minify_styles(cb){
+	gulp.src('assets/styles/css/dist/app.css')
+	.pipe(concat('allstyles.css'))
+	.pipe(gulp.dest('assets/styles/css/dist'))
+	.pipe(rename('allstyles.min.css'))
+	.pipe(cleanCSS({compatibility: 'ie8'}))
+	.pipe(gulp.dest('assets/styles/css/dist'));
 	cb();
 }
 
 
 exports.build = build;
-exports.minify = series(minify_js);
+exports.minify = series(minify_js, minify_styles);
 exports.default = series(clean, build);
--- partials/assets.php
+++ partials/assets.php
@@ -1,2 +1,18 @@
-<script src="./assets/js/global/dist/app.js" type="text/javascript"></script>
-<link href="./assets/styles/css/dist/app.css" rel="stylesheet">
+<?php 
+$whitelist = array(
+    '127.0.0.1',
+    '::1',
+);
+
+if(!in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
+	?>
+	<script src="./assets/js/global/dist/allscripts.min.js" type="text/javascript"></script>
+	<link href="./assets/styles/css/dist/allstyles.min.css" rel="stylesheet">
+	<?php
+}else{?>
+	
+	<script src="./assets/js/global/dist/app.js" type="text/javascript"></script>
+	<link href="./assets/styles/css/dist/app.css" rel="stylesheet">
+<?php 
+}
+?>
